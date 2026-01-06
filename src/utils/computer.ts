940e036a346e519991eebf5f72e710a54a9cd993
Ray-AS <abisuthans.business@gmail.com>
2026-01-06 18:02:22 -0500

    make random attack more efficient

--- src/utils/computer.ts
+++ src/utils/computer.ts
@@ -1,33 +1,59 @@
 import { DEFAULT_BOARD_SIZE } from "../configs";
 import type { Position } from "../models";
-import { Player } from "./player";
+import { Player, SHIPS } from "./player";
 
 export class Computer extends Player {
   // Keep track of previously hit positions to ensure no repetition
-  constructor(private prevHit: Position[] = []) {
+
+  constructor(
+    // private prevHits: Position[] = [],
+    private availableHits: Position[] = [],
+    private shipsToSink = SHIPS.map((s) => s.ship)
+  ) {
     super();
+    for (let y = 0; y < DEFAULT_BOARD_SIZE; y++) {
+      for (let x = 0; x < DEFAULT_BOARD_SIZE; x++) {
+        this.availableHits.push({ x, y });
+      }
+    }
   }
 
   // Random attack choosing for now (TODO: implement smarter algorithm)
+  // chooseAttackRandom(): Position {
+  //   if (this.prevHits.length >= DEFAULT_BOARD_SIZE ** 2) {
+  //     throw new Error("All positions exhausted");
+  //   }
+
+  //   let cellPosition: Position;
+
+  //   do {
+  //     cellPosition = {
+  //       x: Math.floor(Math.random() * DEFAULT_BOARD_SIZE),
+  //       y: Math.floor(Math.random() * DEFAULT_BOARD_SIZE),
+  //     };
+  //   } while (
+  //     this.prevHits.some(
+  //       (position) =>
+  //         position.x === cellPosition.x && position.y === cellPosition.y
+  //     )
+  //   );
+
+  //   this.prevHits.push(cellPosition);
+  //   return cellPosition;
+  // }
+
   chooseAttackRandom(): Position {
-    if (this.prevHit.length >= DEFAULT_BOARD_SIZE**2) {
+    if (this.availableHits.length <= 0) {
       throw new Error("All positions exhausted");
     }
 
-    let cellPosition: Position;
+    const index = Math.floor(Math.random() * this.availableHits.length);
+    const cellPosition = this.availableHits[index];
 
-    do {
-      cellPosition = {
-        x: Math.floor(Math.random() * DEFAULT_BOARD_SIZE),
-        y: Math.floor(Math.random() * DEFAULT_BOARD_SIZE),
-      };
-    } while (
-      this.prevHit.some(
-        (position) => position.x === cellPosition.x && position.y === cellPosition.y
-      )
-    );
+    this.availableHits.splice(index, 1);
 
-    this.prevHit.push(cellPosition);
     return cellPosition;
   }
+
+  chooseAttack() {}
 }
